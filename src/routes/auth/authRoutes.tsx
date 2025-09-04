b93f19affc99c403324bb015edc5c07ed842a822
adolfodev10 <adolfomanueldev10@gmail.com>
2025-09-04 01:43:12 +0100

    feat: add NotFound component and update AuthRoutes to include 404 route

--- src/routes/auth/authRoutes.tsx
+++ src/routes/auth/authRoutes.tsx
@@ -1,21 +1,27 @@
 import { Login } from "@/pages/auth/login";
 import { Register } from "@/pages/auth/register";
 import { AuthLayout } from "@/pages/layout/authLayout";
+import { NotFound } from "@/pages/not-found";
 
 export const AuthRoutes: IRouteProps = {
-    path: "/auth",
-    element: AuthLayout,
-    visibility: "auth",
-    children: [
-      {
-        path: "/auth/login",
-        element: Login,
-        visibility: "auth",
-      },
-      {
-        path: "/auth/register",
-        element: Register,
-        visibility: "auth",
-      },
-    ],
-}
+  path: "/auth",
+  element: AuthLayout,
+  visibility: "auth",
+  children: [
+    {
+      path: "/auth/login",
+      element: Login,
+      visibility: "auth",
+    },
+    {
+      path: "/auth/register",
+      element: Register,
+      visibility: "auth",
+    },
+    {
+      path: "*",
+      element: NotFound,
+      visibility: "auth",
+    },
+  ],
+};
