d194499503171e3ee94ea0bda5bb8b922cd1ab76
adolfodev10 <adolfomanueldev10@gmail.com>
2025-09-04 01:36:12 +0100

    fix: correct formatting and add missing semicolons in AuthLayout component

--- src/pages/layout/authLayout.tsx
+++ src/pages/layout/authLayout.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 export const AuthLayout = () => {
-    return(
-        <div className="w-full flex m-auto justify-around">
-            <Outlet />
-        </div>
-    )
-}
+  return (
+    <div className="w-full flex m-auto justify-around">
+      <Outlet />
+    </div>
+  );
+};
